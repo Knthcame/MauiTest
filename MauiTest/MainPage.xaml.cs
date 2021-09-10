b15aac58e3cd870090f1724687115d5348756d0a
Kenneth Cartañá <kenneth.cartana@ypsomed.com>
2021-09-10 10:00:16 +0200

    Initial commit

---
+++ MauiTest/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiTest
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		int count = 0;
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			count++;
+			CounterLabel.Text = $"Current count: {count}";
+		}
+	}
+}
---
+++ MauiTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace MauiTest
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
